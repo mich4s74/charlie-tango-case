54de81f696a356512d7f3d710442f7987f3ff732
Michael <mich4s74@stud.kea.dk>
2023-05-01 15:30:41 +0200

    third page

--- src/pages/potential_buyers/index.js
+++ src/pages/potential_buyers/index.js
@@ -1,17 +1,56 @@
 import Head from "next/head";
-import styles from "../Home.module.css";
-import house_frontpage from "@/assets/house_frontpage.jpg";
-import Image from "next/image";
+import styles from "./Potential_buyers.module.css";
 
 export default function potential_buyers(props) {
+  const data = props.sellerInfo.selected;
+  console.log(props.sellerInfo.selected);
+
   return (
     <>
       <Head>
         <title>Potential Buyers</title>
       </Head>
-      <div className="">
-        <h1 className={styles.headline}>Find Buyers for your property</h1>
-        <pre>{JSON.stringify(props.sellerInfo, null, 2)}</pre>
+      <div className="wrapper">
+        <h1 className={styles.headline}>Contact potential buyers</h1>
+        <div className={styles.content}>
+          {data.map((oneBuyer) => (
+            <article key={oneBuyer.id}>
+              <h3>BUYER ID: {oneBuyer.id}</h3>
+              <p>Description: {oneBuyer.description}</p>
+            </article>
+          ))}
+        </div>
+      </div>
+
+      <div className="wrapper">
+        <div className={styles.grid_container}>
+          <div className={styles.content}>
+            <form action="/contact" method="GET" className={styles.form}>
+              <label>
+                <span className={styles.label}>Name</span>
+                <input type="text" name="name" required />
+              </label>
+              <label>
+                <span className={styles.label}>Email</span>
+                <input type="text" name="email" required />
+              </label>
+              <label>
+                <span className={styles.label}>Phone</span>
+                <input type="number" name="phone" required />
+              </label>
+              <label>
+                <br />
+                <br />
+                <input type="checkbox" value="consent" />
+                <span className={styles.checkbox}>
+                  Yes please, EDC may contact me with offers and information
+                  related to the real estate market
+                </span>
+              </label>
+              <button className={styles.button}>Contact buyers</button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
