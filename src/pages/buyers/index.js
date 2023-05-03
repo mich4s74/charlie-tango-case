2ecca8607d3c34b1a070a62ea17341229bc0f1e8
Michael <mich4s74@stud.kea.dk>
2023-05-03 11:00:20 +0200

    api url

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -7,7 +7,7 @@ import { useState } from "react";
 export async function getServerSideProps(context) {
   console.log(context);
   const res = await fetch(
-    `charlie-tango-case-ten.vercel.app/api/find-buyers?zipCode=${context.query.zipCode}&price=${context.query.price}&estateType=${context.query.estateType}&size=${context.query.size}`
+    `https://charlie-tango-case-ten.vercel.app/api/find-buyers?zipCode=${context.query.zipCode}&price=${context.query.price}&estateType=${context.query.estateType}&size=${context.query.size}`
   );
 
   const data = await res.json();
